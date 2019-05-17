704008b4949b30c7a39b2c066cc9f70a407d4d64
ReVanTis <revantis0@gmail.com>
2019-05-17 11:37:26 +0800

    Redefine BACKLIGHT_LEVELS 3
    Add adjust layer with numpad like support.
    
    Refine TYPEBLINK code, make backlight blink too.

--- keymap.c
+++ keymap.c
@@ -24,6 +24,7 @@
 #define _LW 1
 #define _RS 2
 #define _FN 3
+#define _AD 4
 
 // Custom Keycode Declarations
 enum {
@@ -135,8 +136,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_NLCK, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,            KC_F12,  _______, ___T___  },
   { KC_SLCK, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,            KC_F24,  KC_PAUS, KC_PSCR  },
   { KC_CAPS, KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_ACL0, KC_ACL2, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, MAGIC_TOGGLE_NKRO, ___T___, ___T___, ___T___  },
-  { RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, BL_TOGG, BL_INC,  BL_DEC,  TYPEBLINK,         KC_WH_U, KC_MS_U, KC_WH_D  },
+  { RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, BL_TOGG, TYPEBLINK,         KC_WH_U, KC_MS_U, KC_WH_D  },
   { _______, RESET  , _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______, _______,           KC_MS_L, KC_MS_D, KC_MS_R  },
+ },
+ 
+ /* ADJUST = RAISE + LOWER
+ * .---------------------------------------------------------------------------------------------------------------------- 2u ------------.
+ * |        | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | +      | ENT    |        | XXXXXX .        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * |        |        |        |        |        |        |        | 4      | 5      | 6      | -      | ENT    |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ------------+--------|
+ * |        | F1     | F2     | F3     | F4     | F5     | F6     | 1      | 2      | 3      | *      | ENT    | XXXXXX .        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * |        | F7     | F8     | F9     | F10    | F11    | F12    | 0      | 0      | .      | /      | ENT    |  HOME  |        |  END   |
+ * |--------+--------+--------+--------+--------+- 2u ------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * |        |        |        |        |        | XXXXXX .        |        |        |        |        |        |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+ 
+ [_AD] = { /* ADJUST = RAISE + LOWER */
+  { _______ , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_PLUS,     KC_KP_ENTER, _______, _______, ___T___  },
+{LCA(KC_TAB), _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_MINUS,    KC_KP_ENTER, _______, _______, _______  },
+  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_ASTERISK, KC_KP_ENTER, ___T___, ___T___, ___T___  },
+  { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_KP_0, KC_KP_0, KC_KP_DOT, KC_KP_SLASH,    KC_KP_ENTER, KC_HOME, _______, KC_END   },
+  { _______ , _______, _______, _______, _______, _______, KC_ENT , _______, _______, _______,   _______,        _______,     _______, _______, _______  },
  },
 };
 
@@ -144,6 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define COLOR_LW         RGB_MAGENTA
 #define COLOR_FN         RGB_CORAL
 #define COLOR_CAPS       RGB_RED
+#define COLOR_AD         RGB_RED
 #define COLOR_MOD        RGB_CYAN
 #define COLOR_TYPEBLINK  RGB_WHITE
 #define COLOR_CUR        rgblight_config.hue,rgblight_config.sat,rgblight_config.val
@@ -154,6 +178,12 @@ int mod_pressed = 0;
 int type_pressed = 0;
 
 extern rgblight_config_t rgblight_config;
+
+#ifdef BACKLIGHT_ENABLE
+extern backlight_config_t backlight_config;
+int origin_level = 0;
+#endif
+
 void led_set_user(uint8_t usb_led) {
     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
         capslock_led_on();
@@ -178,6 +208,9 @@ void set_rgblight_by_layer(uint32_t layer)
         break;
     case _FN:
         rgblight_setrgb(COLOR_FN);
+        break;
+    case _AD:
+        rgblight_setrgb(COLOR_AD);
         break;
     default:
         if (CAPS_ON)
@@ -247,17 +280,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
     default:
         if(user_config.typeblink_on) {
             if (record -> event.pressed) {
+                if(type_pressed == 0)
+                {
+                    rgblight_setrgb(COLOR_TYPEBLINK);
+#ifdef BACKLIGHT_ENABLE
+                    origin_level=backlight_config.level;
+                    if(backlight_config.enable)
+                    {
+                        if(origin_level==BACKLIGHT_LEVELS)
+                            backlight_level(1);
+                        else
+                            backlight_level(BACKLIGHT_LEVELS);
+                    }
+#endif
+                }
                 type_pressed++;
-                rgblight_setrgb(COLOR_TYPEBLINK);
             } else {
                 type_pressed--;
-                if (type_pressed == 0 && mod_pressed == 0)
+                if (type_pressed == 0)
                 {
-                    set_rgblight_by_layer(layer_state);
-                }
-                else if (type_pressed == 0 && mod_pressed != 0)
-                {
-                    rgblight_setrgb(COLOR_MOD);
+                    if(mod_pressed == 0)
+                        set_rgblight_by_layer(layer_state);
+                    else
+                        rgblight_setrgb(COLOR_MOD);
+#ifdef BACKLIGHT_ENABLE
+                    if(backlight_config.enable)
+                        backlight_level(origin_level);
+#endif
                 }
             }
         }
@@ -266,12 +315,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
+    state = update_tri_layer_state(state, _LW, _RS, _AD);
     set_rgblight_by_layer(state);
     switch (biton32(state)) {
     // Lit up GP100 LED with non-default layer.
     case _RS:
     case _LW:
     case _FN:
+    case _AD:
         gp100_led_on();
         break;
     default:
