6b8c85e53019bc51926a379ddc6744b262977f22
ReVanTis <revantis0@gmail.com>
2019-08-13 18:11:29 +0800

    Add key code for send frequent used password string.

--- keymap.c
+++ keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "password.h"
 
 // Fillers to make layering more clear
 #define ___T___ KC_TRNS
@@ -31,6 +32,7 @@ enum {
     BACKLIT = SAFE_RANGE,
     //blink rgb underglow with typing key presses
     TYPEBLINK,
+    PASSWD,
 };
 
 // Tap Dance Declarations
@@ -155,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
  [_AD] = { /* ADJUST = RAISE + LOWER */
-  { _______ , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_PLUS,     KC_KP_ENTER, _______, _______, ___T___  },
+  { PASSWD  , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_PLUS,     KC_KP_ENTER, _______, _______, ___T___  },
 {LCA(KC_TAB), _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_MINUS,    KC_KP_ENTER, _______, _______, _______  },
   { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_ASTERISK, KC_KP_ENTER, ___T___, ___T___, ___T___  },
   { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_KP_0, KC_NLCK, KC_KP_DOT, KC_KP_SLASH,    KC_KP_ENTER, KC_HOME, _______, KC_END   },
@@ -223,6 +225,13 @@ void set_rgblight_by_layer(uint32_t layer)
 
 bool process_record_user(uint16_t keycode, keyrecord_t * record) {
     switch (keycode) {
+    case PASSWD:
+        if (record -> event.pressed) {}
+        else
+        {
+            SEND_STRING(PASSWORD);
+        }
+        break;
 /*
     // Hold BACKLIT = Left Ctrl
     case BACKLIT:
@@ -314,6 +323,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
         }
         return true;
     }
+    return true;
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
