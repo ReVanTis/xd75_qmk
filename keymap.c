3eca160a5736c237f036a7d4c494812d607cbb05
ReVanTis <revantis0@gmail.com>
2019-08-13 18:36:03 +0800

    Add Pin code input string.

--- keymap.c
+++ keymap.c
@@ -24,6 +24,10 @@
 #define PASSWORD "Made by ReVanTis"
 #endif
 
+#ifndef PINCODE
+#define PINCODE "This is a pin code"
+#endif
+
 // Fillers to make layering more clear
 #define ___T___ KC_TRNS
 
@@ -40,6 +44,7 @@ enum {
     //blink rgb underglow with typing key presses
     TYPEBLINK,
     PASSWD,
+    PINCD,
 };
 
 // Tap Dance Declarations
@@ -165,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
  [_AD] = { /* ADJUST = RAISE + LOWER */
   { PASSWD  , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_PLUS,     KC_KP_ENTER, _______, _______, ___T___  },
-{LCA(KC_TAB), _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_MINUS,    KC_KP_ENTER, _______, _______, _______  },
+  { PINCD   , _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_MINUS,    KC_KP_ENTER, _______, _______, _______  },
   { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_ASTERISK, KC_KP_ENTER, ___T___, ___T___, ___T___  },
   { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_KP_0, KC_NLCK, KC_KP_DOT, KC_KP_SLASH,    KC_KP_ENTER, KC_HOME, _______, KC_END   },
   { _______ , _______, _______, _______, _______, _______, KC_ENT , _______, _______, _______,   _______,        _______,     _______, _______, _______  },
@@ -237,6 +242,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
         else
         {
             SEND_STRING(PASSWORD);
+        }
+        break;
+    case PINCD:
+        if (record -> event.pressed) {}
+        else
+        {
+            SEND_STRING(PINCODE);
         }
         break;
 /*
