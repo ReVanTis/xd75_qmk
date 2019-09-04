1f0b155b7feeb8a0cb93c986f6903545e7f02ea1
ReVanTis <revantis0@gmail.com>
2019-09-04 13:10:28 +0300

    Replace RGUI with APP key.

--- keymap.c
+++ keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,          KC_Q,     KC_W,    KC_E,    KC_R,           KC_T,    KC_Y,    KC_U,           KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_INS , KC_DEL   },
   {TD(TD_ESC_CAPS),  KC_A,     KC_S,    KC_D,    KC_F,           KC_G,    KC_H,    KC_J,           KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_ENT , KC_SFTENT},
   { KC_LSFT,         KC_BSPC,  KC_Z,    KC_X,    KC_C,           KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_PGUP_HOME), KC_UP,   TD(TD_PGDN_END)  },
-  { KC_LCTL,         KC_LCTL,  KC_LGUI, KC_LALT, MO(_RS),        KC_SPC,  KC_SPC,  MO(_LW),        KC_RALT, KC_RGUI, MO(_FN), KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT  },
+  { KC_LCTL,         KC_LCTL,  KC_LGUI, KC_LALT, MO(_RS),        KC_SPC,  KC_SPC,  MO(_LW),        KC_RALT, KC_APP , MO(_FN), KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT  },
  },
 
 /* LOWERED
