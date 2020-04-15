e670bf341532027e2316ac19faf2701c458b3d64
ReVanTis <revantis0@gmail.com>
2020-04-15 11:03:13 +0800

    Update keymap like Preonic.

--- keymap.c
+++ keymap.c
@@ -43,7 +43,11 @@
 
 // Custom Keycode Declarations
 enum {
-    BACKLIT = SAFE_RANGE,
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE,
+    FN,
+    BACKLIT,
     //blink rgb underglow with typing key presses
     TYPEBLINK,
     PASSWD,
@@ -86,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_GRV,          KC_1,     KC_2,    KC_3,    KC_4,           KC_5,    KC_6,    KC_7,           KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSLS, KC_BSPC  },
-  { KC_TAB,          KC_Q,     KC_W,    KC_E,    KC_R,           KC_T,    KC_Y,    KC_U,           KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_INS , KC_DEL   },
-  {TD(TD_ESC_CAPS),  KC_A,     KC_S,    KC_D,    KC_F,           KC_G,    KC_H,    KC_J,           KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_ENT , KC_SFTENT},
-  { KC_LSFT,         KC_BSPC,  KC_Z,    KC_X,    KC_C,           KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_PGUP_HOME), KC_UP,   TD(TD_PGDN_END)  },
-  { KC_LCTL,         KC_LCTL,  KC_LGUI, KC_LALT, MO(_RS),        KC_SPC,  KC_SPC,  MO(_LW),        KC_RALT, KC_APP , MO(_FN), KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT  },
+  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_MINS, KC_NLCK, KC_EQL   },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_KP_7, KC_KP_8, KC_KP_9  },
+  { KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_KP_4, KC_KP_5, KC_KP_6  },
+  { KC_LSFT, KC_COMM, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT , KC_UP,   KC_ENT,  KC_KP_1, KC_KP_2, KC_KP_3  },
+  { KC_LCTL, TT(_FN), KC_LGUI, KC_LALT, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT, KC_KP_0, KC_KP_0, KC_PDOT  },
  },
 
 /* LOWERED
@@ -108,11 +112,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
  [_LW] = { /* LOWERED */
-  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,            KC_F8,   KC_F9,           KC_F10,  KC_F11,  KC_F12,  _______, ___T___  },
-{LCA(KC_TAB), _______, _______, _______, _______, _______, _______, TD(TD_PGUP_HOME), KC_UP  , TD(TD_PGDN_END), KC_HOME, _______, _______, _______, _______  },
-  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_LEFT,          KC_DOWN, KC_RGHT,         KC_END , _______, ___T___, ___T___, ___T___  },
-  { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,          KC_DEL , _______,         _______, _______, KC_HOME, _______, KC_END   },
-  { _______ , _______, _______, _______, _______, _______, KC_ENT , _______,          _______, _______,         _______, _______, _______, _______, _______  },
+  { KC_MUTE, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC, _______, _______, _______},
+  { KC_GRV , _______, _______, _______, _______, _______, _______, _______, KC_UP  , KC_LBRC, KC_RBRC, KC_INS,  _______, _______, _______},
+  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______},
+  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_PGUP, _______, _______, _______, _______},
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______},
  },
 
 /* RAISED
@@ -130,11 +134,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
  [_RS] = { /* RAISED */
-  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,            KC_F8,   KC_F9,           KC_F10,  KC_F11,  KC_F12,  _______, ___T___  },
-{LCA(KC_TAB), _______, _______, _______, _______, _______, _______, TD(TD_PGUP_HOME), KC_UP  , TD(TD_PGDN_END), KC_HOME, _______, _______, _______, _______  },
-  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_LEFT,          KC_DOWN, KC_RGHT,         KC_END , _______, ___T___, ___T___, ___T___  },
-  { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_BSPC,          KC_DEL , _______,         _______, _______, KC_HOME, _______, KC_END   },
-  { _______ , _______, _______, _______, _______, _______, KC_ENT , _______,          _______, _______,         _______, _______, _______, _______, _______  },
+  { KC_MPLY, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_BSPC, _______, _______, _______ },
+  { KC_TILD, _______, _______, _______, _______, _______, _______, _______, KC_UP  , KC_LCBR, KC_RCBR, KC_INS,  _______, _______, _______ },
+  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______ },
+  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_PGUP, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______ },
  },
  
 /* FUNCTION
@@ -152,11 +156,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
  [_FN] = { /* FUNCTION */
-  { KC_NLCK, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,            KC_F12,  _______, ___T___  },
-  { KC_SLCK, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,            KC_F24,  KC_PAUS, KC_PSCR  },
-  { KC_CAPS, KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_ACL0, KC_ACL2, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, MAGIC_TOGGLE_NKRO, ___T___, ___T___, ___T___  },
-  { RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, BL_STEP, TYPEBLINK,         KC_WH_U, KC_MS_U, KC_WH_D  },
-  { _______, RESET  , _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______, _______,           KC_MS_L, KC_MS_D, KC_MS_R  },
+  { KC_BTN3, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______ },
  },
  
  /* ADJUST = RAISE + LOWER
@@ -174,11 +178,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
  [_AD] = { /* ADJUST = RAISE + LOWER */
-  { PASSWD  , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_PLUS,     KC_KP_ENTER, _______, _______, ___T___  },
-  { PINCD   , _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_MINUS,    KC_KP_ENTER, _______, _______, _______  },
-  { _______ , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_ASTERISK, KC_KP_ENTER, ___T___, ___T___, ___T___  },
-  { _______ , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_KP_0, KC_NLCK, KC_KP_DOT, KC_KP_SLASH,    KC_KP_ENTER, KC_HOME, _______, KC_END   },
-  { REPTOG  , _______, _______, _______, _______, _______, KC_ENT , _______, _______, _______,   _______,        _______,     _______, _______, _______  },
+  { RGB_TOG, PASSWD , PINCD,   _______, _______, RGB_HUD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { BL_TOGG, RESET,   DEBUG,   _______, _______, RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { KC_CAPS, _______, _______, _______, _______, RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, BL_DEC , BL_INC , _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
  },
 };
 
